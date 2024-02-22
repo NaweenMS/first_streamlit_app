af14b9b20cb163ccd3f8fc24498d398f5ab79d76
NaweenMS <39119507+NaweenMS@users.noreply.github.com>
2024-02-22 12:10:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Helthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Bread and Jam')
-streamlit.text('Poha with Peanut')
-streamlit.text('milk shake with dry fruites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
