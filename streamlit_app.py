74ef6d46b32dee20e87e46e6b701ce90906c7f2d
NaweenMS <39119507+NaweenMS@users.noreply.github.com>
2024-02-22 12:08:42 -0600

    Adding details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
-streamlit.title("My first app")
+streamlit.title('My Parents New Helthy Dinner')
+Streamlit.header('Breakfast Menu')
+Streamlit.text('Bread and Jam')
+Streamlit.text('Poha with Peanut')
+Streamlit.text('milk shake with dry fruites')
